91fe24bdfa6c879c5dc5d97990a074f6bd27314b
sunbrun1 <dkdlrk111@naver.com>
2022-02-07 16:45:31 +0900

    interface - MenuMapper - getDrinkList() 추가

--- domino/src/main/java/com/mycompany/mapper/MenuMapper.java
+++ domino/src/main/java/com/mycompany/mapper/MenuMapper.java
@@ -2,8 +2,11 @@ package com.mycompany.mapper;
 
 import java.util.List;
 
+import com.mycompany.domain.DrinkDTO;
 import com.mycompany.domain.PizzaDTO;
 
 public interface MenuMapper {
 	public List<PizzaDTO> getPizzaList();
+	
+	public List<DrinkDTO> getDrinkList();
 }	
