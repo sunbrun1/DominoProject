391a6a5daea9efca147f4da5180083b178625362
sunbrun1 <dkdlrk111@naver.com>
2022-02-07 16:44:00 +0900

    interface - MenuService - getDrinkList() 추가

--- domino/src/main/java/com/mycompany/service/MenuService.java
+++ domino/src/main/java/com/mycompany/service/MenuService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.mycompany.domain.DrinkDTO;
 import com.mycompany.domain.PizzaDTO;
 
 public interface MenuService {
 	public List<PizzaDTO> getPizzaList();
+	
+	public List<DrinkDTO> getDrinkList();
 }
