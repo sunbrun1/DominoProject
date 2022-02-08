2470c7260d83ae3d6760b7515590f760a15e5d4c
sunbrun1 <dkdlrk111@naver.com>
2022-02-08 15:26:33 +0900

    토핑 ajax 통신 - getToppList 추가

--- domino/src/main/java/com/mycompany/controller/OrderController.java
+++ domino/src/main/java/com/mycompany/controller/OrderController.java
@@ -1,12 +1,20 @@
 package com.mycompany.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.mycompany.domain.ToppDTO;
 import com.mycompany.service.OrderService;
 import lombok.AllArgsConstructor;
 
@@ -16,15 +24,27 @@ import lombok.AllArgsConstructor;
 public class OrderController {
 	@Autowired
 	private OrderService service;
-	
+
 	@GetMapping("/pizzaList")
 	public void getPizzaList(HttpServletRequest request, Model model) {
 		String pizza_name = request.getParameter("name");
 		model.addAttribute("pizzaList", service.getPizzaList(pizza_name));
 		model.addAttribute("dowList", service.getDowList());
-		model.addAttribute("toppList", service.getToppList());
+		model.addAttribute("toppList", service.getToppList("∏ﬁ¿Œ"));
 		model.addAttribute("sideList", service.getSideList());
 		model.addAttribute("drinkList", service.getDrinkList());
 	}
 
+	@GetMapping("/toppList")
+	public void getToppList(HttpServletRequest request, HttpServletResponse response, Model model) {
+		String topp_type = request.getParameter("topp_type");
+		List<ToppDTO> toppDTO = service.getToppList(topp_type);
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().print(mapper.writeValueAsString(toppDTO));
+		} catch (IOException e) {
+			e.printStackTrace();
+		};
+	}
 }
