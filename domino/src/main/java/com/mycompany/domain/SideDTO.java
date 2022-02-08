4e3ad463584fe28611270bbc9d3797a1bdd35d11
sunbrun1 <dkdlrk111@naver.com>
2022-02-08 12:15:28 +0900

    사이드디시 페이지 - SideDTO 추가

---
+++ domino/src/main/java/com/mycompany/domain/SideDTO.java
@@ -0,0 +1,11 @@
+package com.mycompany.domain;
+
+import lombok.Data;
+
+@Data
+public class SideDTO {
+	private String side_name;
+	private int side_price;
+	private String side_type;
+	private String side_img;
+}
