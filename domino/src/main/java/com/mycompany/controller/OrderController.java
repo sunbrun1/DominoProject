0f0a41990f327873e25d5be40da32bc4a9e05006
sunbrun1 <dkdlrk111@naver.com>
2022-02-08 14:17:20 +0900

    피자주문 페이지 - OrderController - getPizzaList() 추가

---
+++ domino/src/main/java/com/mycompany/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.mycompany.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.mycompany.service.OrderService;
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/order/*")
+@AllArgsConstructor
+public class OrderController {
+	@Autowired
+	private OrderService service;
+	
+	@GetMapping("/pizzaList")
+	public void getPizzaList(HttpServletRequest request, Model model) {
+		String pizza_name = request.getParameter("name");
+		model.addAttribute("pizzaList", service.getPizzaList(pizza_name));
+	}
+
+}
