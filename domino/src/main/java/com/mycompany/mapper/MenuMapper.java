da644e25ef2f13fc21a1fd399c4e5d30a8988271
sunbrun1 <dkdlrk111@naver.com>
2022-02-08 12:14:17 +0900

    사이드디시 페이지 - MenuMapper - getSideList() 추가

--- domino/src/main/java/com/mycompany/mapper/MenuMapper.java
+++ domino/src/main/java/com/mycompany/mapper/MenuMapper.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import com.mycompany.domain.DrinkDTO;
 import com.mycompany.domain.PizzaDTO;
+import com.mycompany.domain.SideDTO;
 
 public interface MenuMapper {
 	public List<PizzaDTO> getPizzaList();
 	
 	public List<DrinkDTO> getDrinkList();
+	
+	public List<SideDTO> getSideList();
 }	
