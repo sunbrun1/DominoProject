99c62ecbc419763eb903e9b3992a1acb07544524
sunbrun1 <dkdlrk111@naver.com>
2022-02-08 14:32:37 +0900

    도우/토핑/사이드디시/음료 mapper 함수 추가

--- domino/src/main/java/com/mycompany/mapper/OrderMapper.java
+++ domino/src/main/java/com/mycompany/mapper/OrderMapper.java
@@ -1,7 +1,21 @@
 package com.mycompany.mapper;
 
+import java.util.List;
+
+import com.mycompany.domain.DowDTO;
+import com.mycompany.domain.DrinkDTO;
 import com.mycompany.domain.PizzaDTO;
+import com.mycompany.domain.SideDTO;
+import com.mycompany.domain.ToppDTO;
 
 public interface OrderMapper {
 	public PizzaDTO getPizzaList(String pizza_name);
+	
+	public List<DrinkDTO> getDrinkList();
+
+	public List<SideDTO> getSideList();
+
+	public List<ToppDTO> getToppList();
+
+	public List<DowDTO> getDowList();
 }	
