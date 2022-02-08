045a144b8c1c231839341897338edd2b65ed5eb9
sunbrun1 <dkdlrk111@naver.com>
2022-02-08 14:26:29 +0900

    도우/토핑 DTO 추가

---
+++ domino/src/main/java/com/mycompany/domain/DowDTO.java
@@ -0,0 +1,10 @@
+package com.mycompany.domain;
+
+import lombok.Data;
+
+@Data
+public class DowDTO {
+	private String dow_name;
+	private int dow_price;
+	private String dow_img;
+}
---
+++ domino/src/main/java/com/mycompany/domain/ToppDTO.java
@@ -0,0 +1,11 @@
+package com.mycompany.domain;
+
+import lombok.Data;
+
+@Data
+public class ToppDTO {
+	private String topp_name;
+	private int topp_price;
+	private String topp_img;
+	private String topp_type;
+}
