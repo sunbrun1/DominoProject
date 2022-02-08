facaf9c22f6633fb3bba45bd0d211b4ae3235f2b
sunbrun1 <dkdlrk111@naver.com>
2022-02-08 14:30:48 +0900

    도우/토핑/사이드디시/음료 조회 서비스 추가

--- domino/src/main/java/com/mycompany/service/OrderService.java
+++ domino/src/main/java/com/mycompany/service/OrderService.java
@@ -1,7 +1,21 @@
 package com.mycompany.service;
 
+import java.util.List;
+
+import com.mycompany.domain.DowDTO;
+import com.mycompany.domain.DrinkDTO;
 import com.mycompany.domain.PizzaDTO;
+import com.mycompany.domain.SideDTO;
+import com.mycompany.domain.ToppDTO;
 
 public interface OrderService {
 	public PizzaDTO getPizzaList(String pizza_name);
+	
+	public List<DrinkDTO> getDrinkList();
+	
+	public List<SideDTO> getSideList();
+	
+	public List<ToppDTO> getToppList();
+	
+	public List<DowDTO> getDowList();
 }
